d46fd4004586b50a5719bb9880f3ed497544ac04
Franco Bulgarelli <franco@mumuki.org>
2017-09-27 16:49:53 -0300

    Adding query hook

--- lib/bash_runner.rb
+++ lib/bash_runner.rb
@@ -8,3 +8,4 @@ end
 require_relative './version_hook'
 require_relative './metadata_hook'
 require_relative './test_hook'
+require_relative './query_hook'
---
+++ lib/query_hook.rb
@@ -0,0 +1,19 @@
+class BashQueryHook < Mumukit::Templates::FileHook
+  isolated true
+
+  def tempfile_extension
+    '.sh'
+  end
+
+  def compile_file_content(r)
+<<bash
+#{r.extra}
+#{(r.cookie || []).join("\n")}
+echo $(#{r.query})
+bash
+  end
+
+  def command_line(filename)
+    "chmod u+x #{filename} && . #{filename}"
+  end
+end
