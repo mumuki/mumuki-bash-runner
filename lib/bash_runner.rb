2b5b177054a527332b1a036bd8fc04aea794eba6
Julián Berbel Alt <julianberbel@hotmail.com>
2020-02-06 16:33:29 -0300

    Add grep to default enabled commands

--- lib/bash_runner.rb
+++ lib/bash_runner.rb
@@ -10,7 +10,7 @@ end
 
 module BashRunner
   REQUIRED_COMMANDS = %w(bash sh).freeze
-  DEFAULT_ENABLED_COMMANDS = %w(cat cp git head ls mkdir mv sed tail touch).freeze
+  DEFAULT_ENABLED_COMMANDS = %w(cat cp git grep head ls mkdir mv sed tail touch).freeze
   ALLOWED_COMMANDS = %w(awk cat cp git grep head ln ls mkdir mv rm rmdir sed tac tail touch wc whoami).freeze
 end
 
